546745fd52ddd44e07e57f436072302180c12986
Lucas Nabas <lucas.nabas@viacesi.fr>
2016-06-13 16:38:18 +0200

    Penser à remplacer les langues par les mouvement sur controller

--- game/controller/src/main/java/controller/Controller.java
+++ game/controller/src/main/java/controller/Controller.java
@@ -65,7 +65,7 @@ public class Controller implements IController {
 	 * @see contract.IController#orderPerform(contract.ControllerOrder)
 	 */
 	public void orderPerform(final ControllerOrder controllerOrder) {
-		switch (controllerOrder) {
+		/*switch (controllerOrder) {
 			case English:
 				this.model.loadMessage("GB");
 				break;
@@ -81,7 +81,7 @@ public class Controller implements IController {
 
 			default:
 				break;
-		}
+		}*/
 	}
 
 }
