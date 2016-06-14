a02b7fa257a74a5f4cf039907385abc89983e617
Maxence <Maxence@Max>
2016-06-14 09:19:17 +0200

    Modification, ajout IEntity (non fini)

---
+++ game/model/src/main/java/model/IEntity.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Maxence
+ *
+ */
+public interface IEntity {
+	
+	private TypeMobile Type;
+	private ISprite Sprite;
+	private int PositionX;
+	private int PositionY;
+	
+	Public bool hit(){
+		
+		
+		
+	}
+	
+	public move(){
+		
+		
+		
+	}
+}
