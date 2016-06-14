b713cca97090e9632b75ffdc7709bd9b63b5d61d
Maxence <Maxence@Max>
2016-06-14 17:25:00 +0200

    ...

--- game/model/src/main/java/mobile/Demon.java
+++ game/model/src/main/java/mobile/Demon.java
@@ -3,7 +3,10 @@
  */
 package mobile;
 
-import immobille.IEntity;
+import java.util.ArrayList;
+
+import contract.Permeability;
+import model.IEntity;
 
 /**
  * @author Maxence
@@ -11,4 +14,31 @@ import immobille.IEntity;
  */
 public class Demon implements IEntity{
 
+	private int c;
+
+
+	public boolean hit() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void move(ArrayList<ArrayList<IEntity>> map, int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Permeability getPermeability() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	public void PosDemon(){
+		c = (int)( Math.random() * ( 1 - (-1) + 1 ) ) + (-1);
+		
+		
+	}
+	
+	
+	
 }
