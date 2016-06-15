fbadb0f83d770507320884c1d7b3474929f8bc9e
Maxence <Maxence@Max>
2016-06-15 09:58:53 +0200

    debut de classe map

---
+++ game/model/src/main/java/model/Map.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package model;
+
+import contract.IEntity;
+
+/**
+ * @author Maxence
+ *
+ */
+public class Map extends IMap{
+	public void kill(){
+		
+		
+	}
+	
+	public void move(){
+		
+				
+	}
+	
+	
+	public IEntity get(){
+		
+				
+	}
+	
+	public int getWidth(){
+		
+		
+	}
+	
+	
+	public int getHeight(){
+		
+		
+		
+	}
+	
+	public IEntity getPlayer(){
+		
+		
+		
+		
+	}
+	
+}
