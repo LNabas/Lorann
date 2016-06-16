d41ddb2a3b48186c000b686aff31bcd3e9800671
Maxence <Maxence@Max>
2016-06-16 10:02:20 +0200

    DAOMap

---
+++ game/model/src/main/java/model/DAOMap.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.CallableStatement;
+
+/**
+ * @author Maxence
+ *
+ */
+public class DAOMap extends DAOEntity<Map> {
+		/**
+		 * Instantiates a new DAO Map.
+		 *
+		 * @param connection
+		 *          the connection
+		 * @throws SQLException
+		 *           the SQL exception
+		 */
+		public DAOMap(final Connection connection) throws SQLException {
+			super(connection);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see model.DAOEntity#create(model.Entity)
+		 */
+		@Override
+		public void LoadMap(final Map entity, int id) {
+			try {
+				final String sql = "{call recup_map(?)}";
+				final CallableStatement call = this.getConnection().prepareCall(sql);
+				call.setInt(3, id);
+				call.execute();
+				final ResultSet resultSet = call.getResultSet();
+				if (resultSet.first()) {
+					view.RessourcesLoader.RessourcesLoader();
+				}
+				return;
+			} catch (final SQLException e) {
+				e.printStackTrace();
+			}
+			return;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see model.DAOEntity#delete(model.Entity)
+		 */
+		@Override
+		public boolean delete(final Map entity) {
+			// Not implemented
+			return false;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see model.DAOEntity#update(model.Entity)
+		 */
+		@Override
+		public boolean update(final Map entity) {
+			// Not implemented
+			return false;
+		}
+
+		@Override
+		public Map find(int id) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Map find(String key) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public boolean create(Map entity) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+	
+	
+	
+}
