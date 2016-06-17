a134fe82a858184e459d2329d90cef4d74327a87
Doc0160 <t160d@gmail.com>
2016-06-17 14:30:12 +0200

    != null

--- game/model/src/main/java/model/Map.java
+++ game/model/src/main/java/model/Map.java
@@ -101,7 +101,7 @@ public class Map extends FAILEntity implements IMap{
 	}
 	public int getXof(IEntity e) {
 		for(int i = 0; i<width*height; i++){
-			if(map[i].equals(e)){
+			if(map[i] != null && map[i].equals(e)){
 				return i%width;
 			}
 		}
@@ -109,7 +109,7 @@ public class Map extends FAILEntity implements IMap{
 	}
 	public int getYof(IEntity e) {
 		for(int i = 0; i<width*height; i++){
-			if(map[i].equals(e)){
+			if(map[i] != null && map[i].equals(e)){
 				return i/width;
 			}
 		}
