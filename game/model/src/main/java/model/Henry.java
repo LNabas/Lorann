99b6bfb967b5caa8f2edbfa17c57a1243e55878b
Doc0160 <t0160d@gmail.com>
2016-06-20 16:52:23 +0200

    Henry

--- game/model/src/main/java/model/Henry.java
+++ game/model/src/main/java/model/Henry.java
@@ -13,21 +13,21 @@ public class Henry extends Demon {
 			boolean found = false;
 			int nx = 0;
 			int ny = 0;
-			int radius = 10;
+			int radius = 3;
 			for(nx = -radius; nx<radius && !found; nx++){
 				for(ny = -radius; ny<radius && !found; ny++){
 					int nnx = nx+x;
 					int nny = ny+y;
 					if(nnx>0 && nnx<map.getWidth() && nny>0 && nny<map.getHeight()
 						&&map.get(x + nx, y + ny) != null
-						&&map.get(x + nx, y + ny).getType()==TypeEntity.DEMON){
+						&&map.get(x + nx, y + ny).getType()==TypeEntity.PLAYER){
 						found = true;		
 					}
 				}
 			}
-			nx=(nx==0) ? 0 : nx/Math.abs(nx);
-			ny=(ny==0) ? 0 : ny/Math.abs(ny);
 			if(found){
+				nx=-((nx==0) ? 0 : nx/Math.abs(nx));
+				ny=-((ny==0) ? 0 : ny/Math.abs(ny));
 				if(map.get(x + nx, y + ny) != null &&map.get(x + nx, y + ny).getType()==TypeEntity.PLAYER){
 					map.get(x + nx, y + ny).die(map);
 				}else if(map.get(x + nx, y + ny) == null){
@@ -40,7 +40,31 @@ public class Henry extends Demon {
 					super.move(map, x, y);
 				}
 			}else{
-				super.move(map, x, y);
+				radius = 10;
+				for(nx = -radius; nx<radius && !found; nx++){
+					for(ny = -radius; ny<radius && !found; ny++){
+						int nnx = nx+x;
+						int nny = ny+y;
+						if(nnx>0 && nnx<map.getWidth() && nny>0 && nny<map.getHeight()
+							&&map.get(x + nx, y + ny) != null
+							&&map.get(x + nx, y + ny).getType()==TypeEntity.DEMON){
+							found = true;		
+						}
+					}
+				}
+				nx=(nx==0) ? 0 : nx/Math.abs(nx);
+				ny=(ny==0) ? 0 : ny/Math.abs(ny);
+				if(map.get(x + nx, y + ny) != null &&map.get(x + nx, y + ny).getType()==TypeEntity.PLAYER){
+					map.get(x + nx, y + ny).die(map);
+				}else if(map.get(x + nx, y + ny) == null){
+					map.move(x, y, x + nx, y + ny);
+				}else if(map.get(x, y + ny) == null){
+					map.move(x, y, x, y + ny);
+				}else if(map.get(x + nx, y) == null){
+					map.move(x, y, x + nx, y);
+				}else{
+					super.move(map, x, y);
+				}
 			}
 		}
 	}
