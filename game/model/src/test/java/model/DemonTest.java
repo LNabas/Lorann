16b6c432ec2c283d5fd9a0f669c57b1930ced894
Maxence <Maxence@Max>
2016-06-20 15:38:51 +0200

    DemonTest

---
+++ game/model/src/test/java/model/DemonTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import contract.IMap;
+import contract.TypeEntity;
+import contract.VisualEntity;
+import junit.framework.TestCase;
+
+/**
+ * @author Maxence
+ *
+ */
+public class DemonTest extends Demon {
+
+	/**
+	 * Test method for {@link model.Demon#hit()}.
+	 */
+	@Test
+	public void testHit()  {
+		assertEquals(true,hit());
+	}
+
+	/**
+	 * Test method for {@link model.Demon#isAlive()}.
+	 */
+	@Test
+	public void testIsAlive() {
+		assertEquals(true,hit());
+	}
+
+}
